6f827859e54075f7e1286a57592b6dbd65d455b0
xdustinface <xdustinfacex@gmail.com>
2020-11-23 12:48:31 +0100

    scripts: Adjust build_bls_library.sh
    - Change repo
    - Build `develop`
    - Also run `make install`

--- scripts/build_bls_library.sh
+++ scripts/build_bls_library.sh
@@ -1,5 +1,5 @@
 GOPATH=$(go env GOPATH)
-BLS_REPO_PATH="${GOPATH}"/src/github.com/quantumexplorer/bls-signatures
+BLS_REPO_PATH="${GOPATH}"/src/github.com/dashpay/bls-signatures
 
 # Install packages for bindings
 GO111MODULE=off go get golang.org/x/tools/cmd/goimports
@@ -7,17 +7,19 @@ GO111MODULE=off go get golang.org/x/lint/golint
 
 # Cleaning previous build
 rm -rf "${BLS_REPO_PATH}"
-mkdir -pv "${GOPATH}"/github.com/quantumexplorer/
+mkdir -pv "${GOPATH}"/github.com/dashpay/
 
 # Cloning bls repo and fetching dependencies
-git clone https://github.com/quantumexplorer/bls-signatures.git "$BLS_REPO_PATH"
+git clone https://github.com/dashpay/bls-signatures.git "$BLS_REPO_PATH"
 cd "$BLS_REPO_PATH"
 git submodule update --init --recursive
+git checkout develop
 
-# Build the bindings
+# Build and install the bindings
 mkdir build
 cd build
 cmake ../
 cmake --build . -- -j 6
 cd "${BLS_REPO_PATH}"/go-bindings
 make
+sudo make install
