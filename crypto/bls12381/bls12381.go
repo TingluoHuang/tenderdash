b743043a78482af386ed4266006dc92e42f7ae9b
xdustinface <xdustinfacex@gmail.com>
2020-11-23 12:49:33 +0100

    crypto: Use dashpay/bls-signatures

--- crypto/bls12381/bls12381.go
+++ crypto/bls12381/bls12381.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"io"
 
-	bls "github.com/quantumexplorer/bls-signatures/go-bindings"
+	bls "github.com/dashpay/bls-signatures/go-bindings"
 
 	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/crypto/tmhash"
